9204f429aef107789e36d3bf73e2abc02bb27ec0
eosti <reidsox1@gmail.com>
2023-09-16 00:06:07 -0400

    prep for review

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
         fontFamily: {
             'display': ['jost'],
             // CHANGE THIS TO ONE OF THE TYPEWRITER FONTS
             'body': ['Routed Gothic'],
         },
-		extend: {},
-	},
-	plugins: [],
+        extend: {},
+    },
+    plugins: [
+        plugin(({ matchUtilities, theme }) => {
+            matchUtilities(
+                {
+                    "animation-delay": (value) => {
+                        return {
+                            "animation-delay": value,
+                        };
+                    },
+                },
+                {
+                    values: theme("transitionDelay"),
+                }
+            );
+        }),
+    ],
 }
